29eb661c8f5ea174edc26334e055fedbb317d4bc
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2016-08-09 10:55:31 +0800

    use  for db select

--- db_connect.php
+++ db_connect.php
@@ -4,7 +4,7 @@ mysql_query("set character set 'utf8'");//..
 mysql_query("set names 'utf8'");//.. 
 #mysql_query("set character set 'gb2312'");//..
 #mysql_query("set names 'gb2312'");//.. 
-if($debug==1)
+if($db==1)
 	$db=mysql_select_db("testbook",$link);
 else
 	$db=mysql_select_db("book",$link);
