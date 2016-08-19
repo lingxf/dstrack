edb927daca165c9c462dde9ab4bb6029c87610df
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2016-08-19 09:34:36 +0800

    db_connect

--- db_connect.php
+++ db_connect.php
@@ -2,8 +2,6 @@
 $link=mysql_connect("localhost","bookweb","book2web");
 mysql_query("set character set 'utf8'");//..
 mysql_query("set names 'utf8'");//.. 
-#mysql_query("set character set 'gb2312'");//..
-#mysql_query("set names 'gb2312'");//.. 
 if($db==1)
 	$db=mysql_select_db("testbook",$link);
 else
