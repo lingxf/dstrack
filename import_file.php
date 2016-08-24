66c7d5341fad66b8458a88af7cde944a5c0c3a99
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2016-08-24 14:19:14 +0800

    add share, remove duplicate

---
+++ import_file.php
@@ -0,0 +1,47 @@
+<html>
+<Title>Import Sharing PPT</Title>
+Import Sharing PPT:
+<?php 
+global $login_id;	
+$login_id="";
+session_name("book");
+session_start();
+if(isset($_SESSION['user']))
+	$login_id=$_SESSION['user'];
+else
+	exit();
+?>
+<br>
+<?php 
+include 'book_lib.php';
+include "db_connect.php";
+
+if(isset($_POST['upload'])){
+	$uploaddir = 'share/';
+	$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+	dprint("Move {$_FILES['userfile']['tmp_name']} to $uploadfile<br>");
+	if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+		dprint("File is valid, and was successfully uploaded.\n");
+	} else {
+		print("Move {$_FILES['userfile']['tmp_name']} to $uploadfile failed<br>");
+		return;
+	}
+}
+
+if ($handle = opendir('share/')) {
+	while (false !== ($file = readdir($handle))) {
+		if($file == '..' || $file == '.')
+			continue;
+		print("<a href='share/$file'>$file</a>\n<br>");
+	}
+	closedir($handle);
+}
+
+?>
+<form enctype="multipart/form-data" action="import_ui.php" method="POST">
+    <input type="hidden" name="MAX_FILE_SIZE" value="128000000" />
+    File: <input name="userfile" type="file" />
+    <input name='upload' type="submit" value="Upload" />
+</form>
+
+<html>
