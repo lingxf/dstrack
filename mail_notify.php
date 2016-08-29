6a821364c709536c444b75f2b423859e048c6f50
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2016-08-29 15:17:52 +0800

    add timeout check

--- mail_notify.php
+++ mail_notify.php
@@ -14,6 +14,8 @@ if($action == 'mail')
 	mail_tbd_list();
 else if($action == 'gen')
 	list_record('', 'approve');
+else if($action == 'check')
+	check_timeout();
 
 function mail_tbd_list(){
 	$reason = "";
@@ -42,5 +44,50 @@ function mail_tbd_list(){
 	mail_html($to, $cc, $subject, $message);
 }
 
+function check_timeout()
+{
+	mail_reminder_day(7);
+	mail_reminder_day(3);
+	mail_reminder_day(1);
+	mail_reminder_day(0);
+}
+
+function mail_reminder_day($remain_days)
+{
+
+	$time = time();
+	$lend_days = 28 - $remain_days;
+	if($remain_days > 0)
+		$cond = "(to_days(now())  - to_days(bdate)) = $lend_days";
+	else
+		$cond = "(to_days(now())  - to_days(bdate)) >= 28";
+
+	$sql = " select record_id, borrower, t1.status, name, email, user_name, adate, bdate,rdate,sdate, t1.book_id from history t1, books t2, member t3 where (t1.status = 2 or t1.status = 3) and $cond and  t1.book_id = t2.book_id and t3.user = t1.borrower order by bdate asc ";
+	$res = mysql_query($sql) or die("Invalid query:" . $sql . mysql_error());
+	while($row=mysql_fetch_array($res)){
+		$record_id = $row['record_id']; 
+		$borrower = $row['user_name']; 
+		$book_id = $row['book_id']; 
+		$name = $row['name']; 
+		$email = $row['email']; 
+		$adate= $row['adate']; 
+		$bdate= $row['bdate']; 
+		$rdate= $row['rdate']; 
+		$sdate= $row['sdate']; 
+		$status = $row['status'];
+		$status_text = "";
+		$blink = "";
+		$i++;
+		$bookname = $name; 
+		$to = $email;
+		$cc = 'yingwang@qti.qualcomm.com';
+		$cc = '';
+		$to = 'xling@qti.qualcomm.com';
+		if($remain_days > 0)
+			mail_html($to, $cc, "your book <$bookname> has $remain_days days left", "$borrower $bookname $bdate");
+		else
+			mail_html($to, $cc, "your book <$bookname> has timeout, please return", "$borrower $bookname $bdate");
+	}
+}
 
 ?>
