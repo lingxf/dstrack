94ee3c6a97b1adafef61c3cc136ef6f1b708c872
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2016-08-31 22:03:19 +0800

    enable mail timeout

--- mail_notify.php
+++ mail_notify.php
@@ -13,6 +13,7 @@ $action = $argv[1];
 if($action == 'mail')
 	mail_tbd_list();
 else if($action == 'gen'){
+	$login_id = 'xling';
 	print("&nbsp;&nbsp;申请：");
 	list_record($login_id, 'approve', " status = 1 ");
 	print("&nbsp;&nbsp;归还：");
@@ -70,6 +71,7 @@ function mail_reminder_day($remain_days)
 
 	$sql = " select record_id, borrower, t1.status, name, email, user_name, adate, bdate,rdate,sdate, t1.book_id from history t1, books t2, member t3 where (t1.status = 2 or t1.status = 3) and $cond and  t1.book_id = t2.book_id and t3.user = t1.borrower order by bdate asc ";
 	$res = mysql_query($sql) or die("Invalid query:" . $sql . mysql_error());
+	$i = 0;
 	while($row=mysql_fetch_array($res)){
 		$record_id = $row['record_id']; 
 		$borrower = $row['user_name']; 
@@ -87,8 +89,6 @@ function mail_reminder_day($remain_days)
 		$bookname = $name; 
 		$to = $email;
 		$cc = 'yingwang@qti.qualcomm.com';
-		$cc = '';
-		$to = 'xling@qti.qualcomm.com';
 		if($remain_days > 0)
 			mail_html($to, $cc, "your book <$bookname> has $remain_days days left", "$borrower $bookname $bdate");
 		else
